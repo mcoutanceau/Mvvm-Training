2aba4fedb674a178d0d83e49b42b7a51f9d0f4df
Marc-André COUTANCEAU <ma.coutanceau@ideine.fr>
2017-02-03 11:31:41 +0100

    Implementation of the Mvvm core project

---
+++ TipCalc/TipCalc.Core/App.cs
@@ -0,0 +1,22 @@
+﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+using TipCalc.Core.Services;
+using TipCalc.Core.ViewModels;
+
+namespace TipCalc.Core
+{
+    /// <summary>
+    /// This class is responsible for providing :
+    ///     - registration of which interfaces and implementations the app uses
+    ///     - registration of which ViewModel the App will show when it starts
+    ///     - control of how ViewModels are located - although most applications normally just use the default implementation of this supplied by the base MvxApplication class.
+    /// </summary>
+    public class App : MvxApplication
+    {
+        public App()
+        {
+            Mvx.RegisterType<ICalculation, Calculation>();//Whenever any code requests an ICalculation reference, then the framework should create a new instance of Calculation.
+            Mvx.RegisterSingleton<IMvxAppStart>(new MvxAppStart<TipViewModel>());//whenever any code requests an IMvxAppStart reference, then the framework should return that same appStart instance.
+        }
+    }
+}
---
+++ TipCalc/TipCalc.Core/ViewModels/TipViewModel.cs
@@ -0,0 +1,62 @@
+﻿using MvvmCross.Core.ViewModels;
+using TipCalc.Core.Services;
+
+namespace TipCalc.Core.ViewModels
+{
+    public class TipViewModel : MvxViewModel
+    {
+        private readonly ICalculation _calculation;
+
+        public TipViewModel(ICalculation calculation)
+        {
+            _calculation = calculation;
+        }
+
+        public override void Start()
+        {
+            _subTotal = 100;
+            _generosity = 10;
+            this.Recalculate();
+        }
+
+        private double _subTotal;
+        public double SubTotal
+        {
+            get { return _subTotal; }
+            set
+            {
+                _subTotal = value;
+                base.RaisePropertyChanged(() => SubTotal);
+                this.Recalculate();
+            }
+        }
+
+        private int _generosity;
+        public int Generosity
+        {
+            get { return _generosity; }
+            set
+            {
+                _generosity = value;
+                base.RaisePropertyChanged(() => Generosity);
+                this.Recalculate();
+            }
+        }
+
+        private double _tip;
+        public double Tip
+        {
+            get { return _tip; }
+            set
+            {
+                _tip = value;
+                base.RaisePropertyChanged(() => Tip);
+            }
+        }
+
+        private void Recalculate()
+        {
+            this.Tip = _calculation.TipAmount(SubTotal, Generosity);
+        }
+    }
+}
